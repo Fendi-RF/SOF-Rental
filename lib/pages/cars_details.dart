93c4d3b3c3fb3165c54f4b6b03f7c31534faf0d6
FendiRF <fendiridho@gmail.com>
2022-05-05 18:46:12 +0700

    added http protocol get

--- lib/pages/cars_details.dart
+++ lib/pages/cars_details.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:car_rental_app_ui/data/cars%20copy.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:car_rental_app_ui/widgets/app_bar.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class CarsDetails extends StatefulWidget {
@@ -20,7 +27,7 @@ class _CarsDetailsState extends State<CarsDetails> {
           themeData: Theme.of(context),
         ),
       ),
-      body: CarsList(size: size),
+      body: CarsFromJsonWidget(),
     );
   }
 }
@@ -78,11 +85,101 @@ class CarsList extends StatelessWidget {
                       'assets/images/golf.png',
                       alignment: Alignment.centerRight,
                     ),
-                  )
+                  ),
                 ],
               ),
             );
           }),
     );
   }
+}
+
+class CarsFromJsonWidget extends StatelessWidget {
+  const CarsFromJsonWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return FutureBuilder(
+      future: dioGetCar(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasData) {
+          List<Cars> cars = snapshot.data;
+          return buildCarList(cars, size);
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
+
+Future<List<Cars>> dioGetCar() async {
+  // var dio = Dio();
+  // final response =
+  //     await dio.get('https://ukk-smk-2022.rahmatwahyumaakbar.com/api/vehicles/',
+  //         options: Options(headers: {
+  //           HttpHeaders.authorizationHeader:
+  //               "Bearer 10|5rglV0srLC2UO5KHLf4pIRE83amvIhBbrpzNMiMH"
+  //         }));
+  // final body = jsonDecode(response.data);
+  // return body.map<Cars>(Cars.fromJson).toList();
+
+  final response = await http.get(
+      Uri.parse('https://ukk-smk-2022.rahmatwahyumaakbar.com/api/vehicles/'),
+      headers: {
+        "Accept": "application/json",
+        "Authorization": "Bearer 10|5rglV0srLC2UO5KHLf4pIRE83amvIhBbrpzNMiMH"
+      });
+  final body = jsonDecode(response.body);
+  return body.map<Cars>(Cars.fromJson).toList();
+}
+
+Widget buildCarList(List<Cars> cars, Size size) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: ListView.builder(
+        itemCount: cars.length,
+        itemBuilder: (BuildContext context, int index) {
+          final car = cars[index];
+          return Container(
+            height: size.height * 0.1,
+            child: Stack(
+              alignment: Alignment.centerLeft,
+              children: [
+                Container(
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text(
+                        car.vehicleName,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  height: size.height * 0.05,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Color(0xff3b22a1),
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 15),
+                          color: Colors.black.withOpacity(0.23),
+                          blurRadius: 18,
+                        )
+                      ]),
+                ),
+                Positioned.fill(
+                    child: Image.network(
+                  car.vehicleImage,
+                  alignment: Alignment.centerRight,
+                )),
+              ],
+            ),
+          );
+        }),
+  );
 }
